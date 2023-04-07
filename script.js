8c00136936fca97c0fb90dc873ec89bf8824eff1
daffbg <130174879+daffbg@users.noreply.github.com>
2023-04-07 23:26:51 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+var typed = new Typed(".typing",{
+    strings: ["Web Designer", "Web Developer", "Youtuber", "Blogger"],
+    typeSpeed: 80,
+    backSpeed: 80,
+
+});
+
+var typed = new Typed(".typing-2",{
+    strings: ["Web Developer", "Web Designer", "Youtuber", "Blogger"],
+    typeSpeed: 100,
+    backSpeed: 60,
+
+});
+     
+    
